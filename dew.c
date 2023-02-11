c81da6f779ccc91ddbd6e020eee4e25a6eb1a2c3
vshowc <vshowc@outlook.com>
2023-02-11 16:27:31 +0800

    🎉 project init

---
+++ dew.c
@@ -0,0 +1,10 @@
+//
+// Created by vshowc on 2023/2/11.
+//
+
+#include "dew.h"
+
+void hello()
+{
+	printf("Hello World");
+}
---
+++ dew.h
@@ -0,0 +1,12 @@
+//
+// Created by vshowc on 2023/2/11.
+//
+
+#include <stdio.h>
+
+#ifndef WHITE_DEW_DEW_H
+#define WHITE_DEW_DEW_H
+
+void hello();
+
+#endif //WHITE_DEW_DEW_H
