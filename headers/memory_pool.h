6221d80313054ce5a9f3bcf00fd70a0198b2c6c2
vshowc <vshowc@outlook.com>
2023-02-13 19:01:21 +0800

    memory_pool.h 内存池头文件修正参数

--- headers/memory_pool.h
+++ headers/memory_pool.h
@@ -1,4 +1,7 @@
 #include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
 
 #ifndef WHITE_DEW_MEMORY_POOL_H
 #define WHITE_DEW_MEMORY_POOL_H
@@ -14,20 +17,21 @@ struct MEMORY_BLOCK
 	void * _p;						// 需要返回给使用者的指针
 	void * _start;					// 内存块的起始地址
 	void * _end;					// 内存块的结束地址
-	struct MEMORY_BLOCK * next;		// 指向下一内存块的指针
+	struct MEMORY_BLOCK * _next;	// 指向下一内存块的指针
+	struct MEMORY_BLOCK * _new;		// 当前内存块链表最新的内存块指针
 };
 
 // 全局内存池
-struct MEMORY_BLOCK * global_memory_pool[MAXIMUM_POOL] = {};
+extern struct MEMORY_BLOCK * global_memory_pool[MAXIMUM_POOL];
 
 // 初始化内存块
-struct MEMORY_BLOCK * init_memory_block();
+struct MEMORY_BLOCK * init_memory_block(int _index);
 
 // 开辟一块内存块
 void * dy_malloc(size_t _size);
 
 // 内存块链表扩容
-void Expansion(const struct MEMORY_BLOCK * _block);
+struct MEMORY_BLOCK * Expansion(struct MEMORY_BLOCK * _block, int _multiple, int _index);
 
 // 释放内存
 void dy_free();
