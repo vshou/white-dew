eec5d7cca6e9b392efc5609c251ed73ad1d4a3c5
vshowc <vshowc@outlook.com>
2023-02-13 21:58:50 +0800

    memory_pool.c 代码注释维护

--- memory_pool.c
+++ memory_pool.c
@@ -4,26 +4,35 @@ struct MEMORY_BLOCK * global_memory_pool[MAXIMUM_POOL] = {};
 
 struct MEMORY_BLOCK * init_memory_block(int _index)
 {
+	// 生成内存块结构体
 	struct MEMORY_BLOCK * _block = (struct MEMORY_BLOCK *) malloc(sizeof(struct MEMORY_BLOCK));
+	// 开辟内存块
 	_block->_start = malloc(MEMORY_BLOCK_SIZE);
+	// 初始化属性
 	_block->_end = _block->_start + MEMORY_BLOCK_SIZE;
 	_block->_p = _block->_start;
 	_block->_next = NULL;
 	_block->_new = NULL;
+	// 头内存块节点加入到容器
 	global_memory_pool[_index] = _block;
 	return _block;
 }
 
 struct MEMORY_BLOCK * Expansion(struct MEMORY_BLOCK * _block, int _multiple, int _index)
 {
+	// 获取需要扩容的大小
 	int _size =  MEMORY_BLOCK_SIZE * _multiple;
+	// 生成内存块结构体
 	struct MEMORY_BLOCK * _block_ = (struct MEMORY_BLOCK *) malloc(sizeof(struct MEMORY_BLOCK));
+	// 开辟内存块
 	_block_->_start = malloc(_size);
+	// 初始化属性
 	_block_->_end = _block_->_start + _size;
 	_block_->_p = _block_->_start;
 	_block_->_next = NULL;
 	_block_->_new = NULL;
 	_block->_next = _block_;
+	// 头内存块节点 _new 属性更新
 	global_memory_pool[_index]->_new = _block_;
 	return _block_;
 }
@@ -34,6 +43,7 @@ void * dy_malloc(size_t _size)
 	int _index = ((int) pthread_self()) % MAXIMUM_POOL;
 	// 从全局内存池拿对应的内存块
 	struct MEMORY_BLOCK * _block = global_memory_pool[_index];
+	// 如果头内存块还没初始化则进行初始化操作
 	if (NULL == _block)
 	{
 		_block = init_memory_block(_index);
@@ -54,8 +64,9 @@ void * dy_malloc(size_t _size)
 		// _size 小于 MEMORY_BLOCK_SIZE, 则开辟 MEMORY_BLOCK_SIZE 大小的内存块
 		_block = Expansion(_block, 1, _index);
 	}
-	// 分配内存
+	// 转移指针
 	void * p = _block->_p;
+	// 指针偏移
 	_block->_p = _block->_p + _size;
 	return p;
 }
@@ -66,6 +77,7 @@ void dy_free()
 	int _index = ((int) pthread_self()) % MAXIMUM_POOL;
 	// 从全局内存池拿对应的内存块
 	struct MEMORY_BLOCK * _block = global_memory_pool[_index];
+	// 过渡用的指针变量
 	struct MEMORY_BLOCK * _block_;
 	int sign = 0;
 	// 循环释放链表内存
@@ -75,10 +87,12 @@ void dy_free()
 		{
 			break;
 		}
+		// 如果是头节点则不释放内存, 只需要修改指针变量即可, 以便复用进而减少不必要的初始化操作
 		if (sign == 0)
 		{
 			_block_ = _block->_next;
 			_block->_p = _block->_start;
+			// 覆盖掉之前内存里的内容, 防止意外发生
 			memset(_block->_start, 0, MEMORY_BLOCK_SIZE);
 			_block->_new = NULL;
 			_block->_next = NULL;
@@ -86,7 +100,9 @@ void dy_free()
 			sign++;
 			continue;
 		}
+		// 传递指针过渡
 		_block_ = _block->_next;
+		// 释放内存
 		free(_block->_start);
 		free(_block);
 		_block = _block_;
